7dbdd860c87f67a5c7d0ef3a88bc510d42b2752e
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-04 21:35:35 +0545

    add : axios instance

--- src/api/PrivateAxios.ts
+++ src/api/PrivateAxios.ts
@@ -1,1 +1,18 @@
 import axios from "axios";
+
+//get token from local storage
+const getToken = () => {
+  return JSON.parse(localStorage.getItem("user") || "{}")?.token;
+};
+
+export default axios
+  .create({
+    baseURL: process.env.REACT_APP_API_URL,
+    timeout: 5000,
+  })
+  .interceptors.request.use((config: any) => {
+    const auth = getToken() ? `${getToken()}` : "";
+    config.headers.common["Authorization"] = auth;
+    config.headers.common["Authorization"] = `Bearer ${getToken()}`;
+    return config;
+  });
