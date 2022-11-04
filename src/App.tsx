4566d73b9edc031e90ceab92a5f18c216d789066
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-04 22:27:51 +0545

    add : routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 // custom scss
 import "./assets/sass/main.scss";
+// toast scss
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+// routes
+import Routes from "./routes";
+
 function App() {
-  return <div className=""></div>;
+  return (
+    <section>
+      <Routes />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop
+        draggable
+        theme="light"
+      />
+    </section>
+  );
 }
 
 export default App;
