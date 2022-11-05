b2b130b49b50c4c2a2b8bae8c4a873dbeebb2864
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-05 10:24:35 +0545

    add : images

--- src/config/constant/Images.ts
+++ src/config/constant/Images.ts
@@ -1,3 +1,8 @@
+import logo from "../../assets/img/logo.png";
+import slider from "../../assets/img/slider.png";
+import product from "../../assets/img/car.jpeg";
 export default {
-    
+  logo,
+  slider,
+  product,
 };
