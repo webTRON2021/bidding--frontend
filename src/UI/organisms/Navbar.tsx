1cb5e8a3a23b81b07f883c1cf33047e436372954
ibijayashah <bijayashah551@gmail.com>
2022-11-05 17:27:53 +0545

    fix : home route

--- src/UI/organisms/Navbar.tsx
+++ src/UI/organisms/Navbar.tsx
@@ -2,19 +2,31 @@ import Button from "../atoms/Button";
 import Input from "../atoms/Input";
 import { BiSearch } from "react-icons/bi";
 import Images from "../../config/constant/Images";
+import Form from "react-bootstrap/Form";
+import { useNavigate, Link } from "react-router-dom";
+
 export default () => {
+  const navigate = useNavigate();
+  // function to search 
+  const handleSubmit = (e: any) => {
+    navigate("/products");
+  }
   return (
     <section className="navbar_section">
       <div className="container-fluid">
         <div className="navbar_box">
-          <div className="logo">
-            <img src={Images.logo} alt="logo" />
-            <div className="title">Tinjure Auction</div>
-          </div>
-          <div className="d-flex gap-2">
-            <Input icon={<BiSearch />} placeholder="Search here"></Input>
-            <Button>Search Bids</Button>
-          </div>
+          <Link to="/">
+            <div className="logo">
+              <img src={Images.logo} alt="logo" />
+              <div className="title">Tinjure Auction</div>
+            </div>
+          </Link>
+          <Form onSubmit={(e) => { handleSubmit(e) }}>
+            <div className="d-flex gap-2">
+              <Input icon={<BiSearch />} placeholder="Search here"></Input>
+              <Button>Search Bids</Button>
+            </div>
+          </Form>
           <div className="">
             <div className="d-flex gap-2">
               <Button variant="primary" loading={false}>
