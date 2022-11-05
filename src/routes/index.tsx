906572547797dc15f5639924d5ec9eba94ddc1e6
ibijayashah <bijayashah551@gmail.com>
2022-11-05 17:30:38 +0545

    add : routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Contact from "../UI/pages/Contact";
+import Details from "../UI/pages/Details";
 import Home from "../UI/pages/Home";
+import Product from "../UI/pages/Product";
 import MainLayout from "../UI/templates/MainLayout";
 export default () => {
   return (
@@ -9,6 +11,8 @@ export default () => {
         <Route path="" element={<MainLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/products" element={<Product />} />
+          <Route path="/product-details" element={<Details />} />
         </Route>
       </Routes>
     </BrowserRouter>
