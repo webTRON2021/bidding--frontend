b2325c494783adf6aec4f9c909bd9c1de4b7a687
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-05 10:25:35 +0545

    add : input

---
+++ src/UI/atoms/Input.tsx
@@ -0,0 +1,36 @@
+import { InputHTMLAttributes, ReactNode, useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  icon?: ReactNode;
+  variant?: string;
+  error?: boolean;
+}
+
+export default ({ icon, children, variant, ...rest }: InputProps) => {
+  // state for store password
+  const [showPassword, setShowPassword] = useState<boolean>(true);
+
+  // function to toggle password
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+  return (
+    <section className="input_section">
+      {variant === "password" ? (
+        <div className="input_box">
+          <div className="icon">{icon}</div>
+          <input type={showPassword ? "password" : "text"} {...rest} />
+          <div className="icon" onClick={togglePassword}>
+            {showPassword ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+          </div>
+        </div>
+      ) : (
+        <div className="input_box">
+          <div className="icon">{icon}</div>
+          <input type={variant} {...rest} />
+        </div>
+      )}
+    </section>
+  );
+};
