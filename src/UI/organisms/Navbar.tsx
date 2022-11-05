df1bf62feb7dca87d07f3aa5a90bf847e764e829
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-05 10:20:02 +0545

    add : navbar

--- src/UI/organisms/Navbar.tsx
+++ src/UI/organisms/Navbar.tsx
@@ -1,3 +1,32 @@
+import Button from "../atoms/Button";
+import Input from "../atoms/Input";
+import { BiSearch } from "react-icons/bi";
+import Images from "../../config/constant/Images";
 export default () => {
-  return <section className="navbar_section">Navbar</section>;
+  return (
+    <section className="navbar_section">
+      <div className="container-fluid">
+        <div className="navbar_box">
+          <div className="logo">
+            <img src={Images.logo} alt="logo" />
+            <div className="title">Tinjure Auction</div>
+          </div>
+          <div className="d-flex gap-2">
+            <Input icon={<BiSearch />} placeholder="Search here"></Input>
+            <Button>Search Bids</Button>
+          </div>
+          <div className="">
+            <div className="d-flex gap-2">
+              <Button variant="primary" loading={false}>
+                Login
+              </Button>
+              <Button variant="primary_white_outline" loading={false}>
+                Registration
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
