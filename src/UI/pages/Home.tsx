2938ad8e5eaa546505716535fa9e2f4c4c6566d8
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-05 10:20:18 +0545

    add : home page

--- src/UI/pages/Home.tsx
+++ src/UI/pages/Home.tsx
@@ -1,3 +1,28 @@
+import Button from "../atoms/Button";
+import Heading from "../atoms/Heading";
+import CardTwo from "../molecules/CardTwo";
+import PopularBids from "../organisms/PopularBids";
+import Slider from "../organisms/Slider";
+
 export default () => {
-  return <section className="page_section">Home page</section>;
+  return (
+    <section className="page_section">
+      <Slider />
+      <div className="container py-4">
+        <PopularBids />
+        <div className="pt-5">
+          <Heading text="More Bids for You" />
+          <div className="pt-3"></div>
+          {[1, 1, 1, 1].map((item, index) => {
+            return <CardTwo />;
+          })}
+          <div className="pt-2 d-flex justify-content-center">
+            <Button variant="secondary" className="p-5" useFor="pagination">
+              Load More
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
