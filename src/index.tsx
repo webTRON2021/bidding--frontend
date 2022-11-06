3d3e4db033cf1deeef28aac5b4017548e40bdc0a
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-06 08:51:55 +0545

    fix: routes and wrap with the AuthContext

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import AuthContext from "./contexts/Auth";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <AuthContext>
     <App />
+    </AuthContext>
   </React.StrictMode>
 );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../UI/pages/Home";
+import Login from "../UI/pages/Login";
+import AuthTemplate from "../UI/templates/AuthTemplate";
 import MainLayout from "../UI/templates/MainLayout";
 export default () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/auth" element={<AuthTemplate />}>
+          <Route path="" element={<Login />} />{" "}
+        </Route>
         <Route path="" element={<MainLayout />}>
           <Route path="/" element={<Home />} />
         </Route>
