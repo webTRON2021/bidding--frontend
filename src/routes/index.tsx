5584a28a02028a86aa8c9b4adf81fcc887000d18
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-06 08:58:57 +0545

    fix: merge conflict

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Contact from "../UI/pages/Contact";
 import Details from "../UI/pages/Details";
 import Home from "../UI/pages/Home";
-<<<<<<< HEAD
 import Login from "../UI/pages/Login";
 import AuthTemplate from "../UI/templates/AuthTemplate";
-=======
 import Product from "../UI/pages/Product";
->>>>>>> 12dd79f828187d61a5c24c65f5fc7533332290a0
 import MainLayout from "../UI/templates/MainLayout";
 export default () => {
   return (
