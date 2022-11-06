db129b8da56f3af8a8fc7ad86afa5b32f3b387b1
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-06 08:41:43 +0545

    add: logo

---
+++ src/UI/molecules/Logo.tsx
@@ -0,0 +1,13 @@
+import image from "../../config/constant/Images";
+
+export default () => {
+  return (
+    <section className="logo_section">
+      <div className="logo">
+        <img src={image.logo} alt="logo" />
+        Tinjure Supplier Auction
+      </div>
+      
+    </section>
+  );
+};
