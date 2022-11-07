3fc6b1afa18040384f198955b61a8e8647b40176
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-07 14:43:36 +0545

    add: db config file

---
+++ src/config/dbConfig.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyADHawk25moqoiB_rcNquYfltjzLGz3Ns4",
+  authDomain: "test-bidding-app.firebaseapp.com",
+  projectId: "test-bidding-app",
+  storageBucket: "test-bidding-app.appspot.com",
+  messagingSenderId: "229717036764",
+  appId: "1:229717036764:web:43ec298cc6bbf7661bae19",
+};
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
