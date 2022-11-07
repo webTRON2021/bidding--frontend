e61fac1f6665c8267f2d27b2d8857f91d87a6b74
Pramesh Karki <prameshkarki0407@gmail.com>
2022-11-08 01:32:42 +0545

    feat: User verification

--- src/api/PrivateAxios.ts
+++ src/api/PrivateAxios.ts
@@ -4,7 +4,6 @@ let user = JSON.parse(localStorage.getItem("user") || "{}");
 
 const PrivateAxiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-    timeout: 5000,
     headers: { Authorization: `Bearer ${user?.token}` },
    
 });
