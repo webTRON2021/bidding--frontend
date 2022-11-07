8f7ce49f378fea2b6035272f690d19eb53bc365a
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-07 19:23:56 +0545

    fix: text for schema

--- src/config/constant/text.ts
+++ src/config/constant/text.ts
@@ -1,4 +1,19 @@
 export const getRequiredText = (text: string) => `${text} is required`;
 export const getValidText = (text: string) => `${text} must be valid`;
 
-export default {};
+export default {
+    EMAIL_VALID: "Enter a valid email",
+  EMAIL_REQUIRED: "Email is required",
+//   PHONE_REQUIRED: "Phone is required",
+//   PHONE_VALID: "Enter a valid phone",
+//   NAME_REQUIRED: "Name is required",
+  PASSWORD_REQUIRED: "Password is  required",
+//   Old_PASSWORD_REQUIRED: "Old Password is  required",
+//   PASSWORD_MATCH: "Both password needs to be same",
+  PASSWORD_SUGGESTION:
+    "Use 8 or more characters with letters, numbers & symbols",
+    // OTP_REQUIRED: "Please Enter 5 digit OTP",
+    // NEW_PASSWORD_REQUIRED: "New password is  required",
+  CONFIRM_PASSWORD_REQUIRED: "Confirm Password is required field",
+
+};
