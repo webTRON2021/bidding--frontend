a28e9206f5989b7d489a2dcee44030b41aa5a116
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-08 01:59:30 +0545

    fix: added active class

--- src/UI/organisms/Navbar2.tsx
+++ src/UI/organisms/Navbar2.tsx
@@ -1,25 +1,40 @@
 import { Nav, Navbar, Container } from "react-bootstrap";
 
-import {Link} from "react-router-dom";
-
-
+import { Link, useLocation } from "react-router-dom";
 
 const NavBar = () => {
+  const location = useLocation();
+  console.log(location.pathname);
   return (
     <section className="navbar_section">
-      
-        <Navbar>
-          <Container fluid>
-                <Nav className="justify-content-start flex-grow-1 pe-3">
-                  <Nav.Link as={Link} to="/">Home</Nav.Link>
-                  <Nav.Link as={Link} to="/add-product">Add Bid</Nav.Link>
-                  <Nav.Link as={Link} to="service">Our Services</Nav.Link>
-                  <Nav.Link as={Link} to="/product">Products</Nav.Link>
-                  <Nav.Link as={Link} to="/contact">Conatct Us</Nav.Link>
-                </Nav>
-          </Container>
-        </Navbar>
-      
+      <Navbar>
+        
+        <Container fluid>
+          <Nav className="justify-content-start flex-grow-1 pe-3">
+            <Nav.Link as={Link} to="/"
+            active={location.pathname==="/"}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/add-product"
+            active={location.pathname==="/add-product"}
+            >
+              Add Bid
+            </Nav.Link>
+
+            <Nav.Link as={Link} to="/product"
+            active={location.pathname==="/product"}
+            >
+              Products
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact"
+            active={location.pathname==="/contact"}
+            >
+              Conatct Us
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </section>
   );
 };
