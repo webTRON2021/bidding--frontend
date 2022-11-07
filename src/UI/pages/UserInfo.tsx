e6b01462b71caaec8e9e5ed752a0f9ba075ecfe4
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-07 23:09:57 +0545

    fix: api fetch for user info

--- src/UI/pages/UserInfo.tsx
+++ src/UI/pages/UserInfo.tsx
@@ -10,12 +10,30 @@ const UserInfo = () => {
     <section className="userinfo_section">
         <div className="container">
             <div className="row d-flex justify-content-between align-items-center">
-                <div className="col-lg-4 col-md-8 col-12">
+                <div className="col-lg-4 col-md-8 col-12 user_image">
                     <img src={Images.logo} alt="logo" />
+                   <div className="user_status">
+                     Status:{user?.info?.status}
+                    </div>
                 </div>
-                <div className="col-lg-8 col-md-8 col-12">
-                    <div className="user_name">
-                        Tinjure Supplier
+                <div className="col-lg-8 col-md-8 col-12 user_details">
+                    <div className="row user_info">
+                        <div className="col-6">
+                        FirstName: {user?.info?.firstName}
+                        </div>
+                        <div className="col-6">
+                        LastName: {user?.info?.lastName}
+                        </div>
+                        <div className="col-6">
+                        Sex: {user?.info?.gender}
+                        </div>
+                        <div className=" col-6">
+                        DOB: {user?.info?.dob}
+                        </div>
+                        <div className="col-6">
+                        RecoveryEmail: {user?.info?.recoveryEmail}
+                        </div>
+                        
                     </div>
                 </div>
                 
