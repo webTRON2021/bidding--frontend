aba606bb3477cbdcf40abd77b5500f2fe8d467c5
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-07 14:46:26 +0545

    fix: integrate api

--- src/UI/pages/Details.tsx
+++ src/UI/pages/Details.tsx
@@ -4,7 +4,17 @@ import Title from "../atoms/Title"
 import { TbCurrencyDollar } from "react-icons/tb"
 import Button from "../atoms/Button"
 import SimilarProduct from "../organisms/SimilarProduct"
+
+import {FaUser} from "react-icons/fa";
+import DetailsRunningBids from "../molecules/DetailsRunningBids"
+import { useLocation } from "react-router-dom"
+import Creative from '../../models/Creative';
+
 export default () => {
+
+    const location = useLocation();
+    const bid = location.state;
+    
     // payment option icon
     const paymentOption = [
         Images.esewa,
@@ -18,31 +28,33 @@ export default () => {
     return <section className="details_page_section py-4">
         <div className="container">
             <div className="row">
-                <div className="col-md-5 col-12">
+                <div className="col-md-3 col-12">
                     <div className="image">
-                        <img src={Images.product} alt="product image" />
+                        <img src={bid.Creative} alt="product image" />
                     </div>
                 </div>
-                <div className="col-md-7 col-12">
+                <div className="col-md-5 col-12">
                     <div className="details">
-                        <Title text="1856 $1 One Dollar Gold Indian Princess Type 3 Upright 5 Ex Mount ? #CC" />
+                        <Title text={bid.name} />
                         <div className="items">
-                            <div className="item">Condition : Good condition</div>
+                            <div className="item">Status:{bid.status}</div>
+                            <div className="item">Brand:{bid.brand}</div>
+                            <div className="item">Specification:{bid.specification}</div>
                             <div className="item">Time Left : 4d | Thursday, 2:15PM</div>
                             <div className="item bid_input py-4 ">
-                                <div>Current bid : <span className="currency">Npr: 30303</span></div>
+                                <div>Initial Bid : <span className="currency">{bid.tokenCurrency}:{bid.initialToken}</span></div>
                                 <div className="pt-2 d-flex gap-2">
                                     <div className="w-25">
                                         <Input type="number" icon={<TbCurrencyDollar />} />
                                     </div>
                                     <Button>Place Bid</Button>
                                 </div>
-                                <div className="pt-1"> minimum increment (Npr. 1000)</div>
+                                <div className="pt-1"> minimum increment (Nrp:{bid.minimumTokenRaise})</div>
                             </div>
                             <div className="item font-weight-500">
                                 Description
+                                <div>{bid.bidStory}</div>
                             </div>
-                            <div className="item">Location : NewBaneshwor-28, Ktm Nepal</div>
                             <div className="item">Delivery : free  within ktm valley </div>
                             <div className="item">Return : 30 day returns | Buyer pays for return shipping  </div>
                             <div className="item pt-2 font-weight-500">
@@ -50,7 +62,7 @@ export default () => {
                             </div>
                             <div className="item d-flex gap-3">
                                 {
-                                    paymentOption && paymentOption.map((item: string, index: number) => {
+                                    paymentOption && paymentOption?.map((item: string, index: number) => {
                                         return <div className="logo" key={index}>
                                             <img src={item} alt="logo" />
                                         </div>
@@ -61,6 +73,9 @@ export default () => {
 
 
                     </div>
+                </div>
+                <div className="details_bids col-md-4 col-12">
+                    <DetailsRunningBids/>
                 </div>
             </div>
         </div>
