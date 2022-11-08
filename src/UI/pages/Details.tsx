ec872dfd71f99ac815d3d88bae2d2695fb15bae5
laxmanpokhrel <laxmanpokhrel102@gmail.com>
2022-11-08 11:58:24 +0545

    first name casing improved

--- src/UI/pages/Details.tsx
+++ src/UI/pages/Details.tsx
@@ -15,145 +15,146 @@ import { toast } from "react-toastify";
 import { useState } from "react";
 import useAuth from "../../hooks/useAuth";
 import Alert from "../atoms/Alert";
+import DetailsRunningBids from "../molecules/DetailsRunningBids";
 
 const Details = () => {
-  const location = useLocation();
-  const bid: Bid = location.state;
-  const { postBidToken } = useBid();
+	const location = useLocation();
+	const bid: Bid = location.state;
+	const { postBidToken } = useBid();
 
-  const { user } = useAuth();
+	const { user } = useAuth();
 
-  // * Define state to store user details
-  const [isVerifiedProfile, setIsVerifiedProfile] = useState(
-    user?.info?.status === "UNVERIFIED" ? false : true
-  );
+	// * Define state to store user details
+	const [isVerifiedProfile, setIsVerifiedProfile] = useState(
+		user?.info?.status === "UNVERIFIED" ? false : true
+	);
 
-  useScrollTop();
+	useScrollTop();
 
-  // payment option icon
-  const paymentOption = [
-    Images.esewa,
-    Images.bank_transfer,
-    Images.imepay,
-    Images.paypal,
-    Images.khalti,
-    Images.visacard,
-  ];
+	// payment option icon
+	const paymentOption = [
+		Images.esewa,
+		Images.bank_transfer,
+		Images.imepay,
+		Images.paypal,
+		Images.khalti,
+		Images.visacard,
+	];
 
-  const placeBidHandler = () => {
-    // ! Do not use this approach in production
-    const bidAmount = document.getElementById("bidAmount") as HTMLInputElement;
-    const bidAmountValue = bidAmount.value;
+	const placeBidHandler = () => {
+		// ! Do not use this approach in production
+		const bidAmount = document.getElementById("bidAmount") as HTMLInputElement;
+		const bidAmountValue = bidAmount.value;
 
-    payment(
-      bid._id,
-      bid.name,
-      `https://tinuresuppliers.com.np/bids/${bid._id}`,
-      (payload: any) => {
-        // *Request data to the server
-        const body = new FormData();
-        body.append("transactionId", payload.token);
-        body.append("bidId", bid._id);
-        body.append("amount", bidAmountValue);
+		payment(
+			bid._id,
+			bid.name,
+			`https://tinuresuppliers.com.np/bids/${bid._id}`,
+			(payload: any) => {
+				// *Request data to the server
+				const body = new FormData();
+				body.append("transactionId", payload.token);
+				body.append("bidId", bid._id);
+				body.append("amount", bidAmountValue);
 
-        postBidToken(body).then((response) => {
-          if (!response.error) {
-            toast.error("Bid placed successfully");
-          } else {
-            toast.error(response.error);
-          }
-        });
-      },
-      (error: any) => {
-        toast.error(error.toString());
-      },
-      () => {}
-    ).show({
-      amount: +bidAmountValue * 100,
-    });
+				postBidToken(body).then((response) => {
+					if (!response.error) {
+						toast.error("Bid placed successfully");
+					} else {
+						toast.error(response.error);
+					}
+				});
+			},
+			(error: any) => {
+				toast.error(error.toString());
+			},
+			() => {}
+		).show({
+			amount: +bidAmountValue * 100,
+		});
 
-    bidAmount.value = "";
-  };
+		bidAmount.value = "";
+	};
 
-  return (
-    <section className="details_page_section py-4">
-      {!isVerifiedProfile && <Alert/>}
-      <div className="container">
-        <div className="row">
-          <div className="col-md-3 col-12">
-            <div className="image">
-              <img src={bid?.creative[0]?.src} alt="product image" />
-            </div>
-          </div>
-          <div className="col-md-4 col-12">
-            <div className="details">
-              <Title text={bid.name} />
-              <div className="items">
-                <div className="item">Status:{bid.status}</div>
-                <div className="item">Brand:{bid.brand}</div>
-                <div className="item">Specification:{bid.specification}</div>
-                <div className="item d-flex align-align-items-center">
-                  Time left:
-                  <span className="display-6">
-                    <Timer endDate={bid.endDate} onTimerEnd={() => {}} />
-                  </span>
-                </div>
-                <div className="item bid_input py-4 ">
-                  <div>
-                    Initial Bid :{" "}
-                    <span className="currency">
-                      {bid.tokenCurrency}:{bid.initialToken}
-                    </span>
-                  </div>
-                  {isVerifiedProfile && (
-                    <div className="pt-2 d-flex gap-2">
-                      <div className="w-25">
-                        <Input
-                          type="number"
-                          icon={<TbCurrencyDollar />}
-                          id="bidAmount"
-                        />
-                      </div>
-                      <Button onClick={placeBidHandler}>Place Bid</Button>
-                    </div>
-                  )}
-                  <div className="pt-1">
-                    {" "}
-                    minimum increment (Nrp:{bid.minimumTokenRaise})
-                  </div>
-                </div>
-                <div className="item font-weight-500">
-                  Description
-                  <div>{bid.bidStory}</div>
-                </div>
-                <div className="item">Delivery : free within ktm valley </div>
-                <div className="item">
-                  Return : 30 day returns | Buyer pays for return shipping{" "}
-                </div>
-                <div className="item pt-2 font-weight-500">Payments</div>
-                <div className="item d-flex gap-3">
-                  {paymentOption &&
-                    paymentOption?.map((item: string, index: number) => {
-                      return (
-                        <div className="logo" key={index}>
-                          <img src={item} alt="logo" />
-                        </div>
-                      );
-                    })}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="details_bids col-md-5 col-12">
-                    <DetailsRunningBids/>
-                </div>
-        </div>
-      </div>
-      <div className="pt-5">
-        <SimilarProduct />
-      </div>
-    </section>
-  );
+	return (
+		<section className='details_page_section py-4'>
+			{!isVerifiedProfile && <Alert />}
+			<div className='container'>
+				<div className='row'>
+					<div className='col-md-3 col-12'>
+						<div className='image'>
+							<img src={bid?.creative[0]?.src} alt='product image' />
+						</div>
+					</div>
+					<div className='col-md-4 col-12'>
+						<div className='details'>
+							<Title text={bid.name} />
+							<div className='items'>
+								<div className='item'>Status:{bid.status}</div>
+								<div className='item'>Brand:{bid.brand}</div>
+								<div className='item'>Specification:{bid.specification}</div>
+								<div className='item d-flex align-align-items-center'>
+									Time left:
+									<span className='display-6'>
+										<Timer endDate={bid.endDate} onTimerEnd={() => {}} />
+									</span>
+								</div>
+								<div className='item bid_input py-4 '>
+									<div>
+										Initial Bid :{" "}
+										<span className='currency'>
+											{bid.tokenCurrency}:{bid.initialToken}
+										</span>
+									</div>
+									{isVerifiedProfile && (
+										<div className='pt-2 d-flex gap-2'>
+											<div className='w-25'>
+												<Input
+													type='number'
+													icon={<TbCurrencyDollar />}
+													id='bidAmount'
+												/>
+											</div>
+											<Button onClick={placeBidHandler}>Place Bid</Button>
+										</div>
+									)}
+									<div className='pt-1'>
+										{" "}
+										minimum increment (Nrp:{bid.minimumTokenRaise})
+									</div>
+								</div>
+								<div className='item font-weight-500'>
+									Description
+									<div>{bid.bidStory}</div>
+								</div>
+								<div className='item'>Delivery : free within ktm valley </div>
+								<div className='item'>
+									Return : 30 day returns | Buyer pays for return shipping{" "}
+								</div>
+								<div className='item pt-2 font-weight-500'>Payments</div>
+								<div className='item d-flex gap-3'>
+									{paymentOption &&
+										paymentOption?.map((item: string, index: number) => {
+											return (
+												<div className='logo' key={index}>
+													<img src={item} alt='logo' />
+												</div>
+											);
+										})}
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className='details_bids col-md-5 col-12'>
+						<DetailsRunningBids />
+					</div>
+				</div>
+			</div>
+			<div className='pt-5'>
+				<SimilarProduct />
+			</div>
+		</section>
+	);
 };
 
 export default Details;
--- src/api/PrivateAxios.ts
+++ src/api/PrivateAxios.ts
@@ -2,10 +2,12 @@ import axios from "axios";
 
 let user = JSON.parse(localStorage.getItem("user") || "{}");
 
+console.log(user.token);
+
 const PrivateAxiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
     
-    headers: { Authorization: `Bearer ${user?.token}` },
+    headers: { Authorization: `Bearer ${user.token}` },
    
 });
 
