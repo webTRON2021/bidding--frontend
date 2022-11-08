7fb42c7e87a74914dad97223b00119313b54b3f5
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-08 15:18:44 +0545

    fix: validation check on sign up form

--- src/config/schema/SignUpSchema.ts
+++ src/config/schema/SignUpSchema.ts
@@ -1,14 +1,25 @@
 import * as Yup from "yup";
-import { getRequiredText } from "../constant/text";
+import text, { getRequiredText } from "../constant/text";
 export const SignUpSchema = Yup.object().shape({
-    firstName: Yup.string().required(getRequiredText("firstName")),
-    lastName: Yup.string().required(getRequiredText("lastName")),
-    password: Yup.string().required(getRequiredText("password")),
-    gender: Yup.string().required(getRequiredText(" gender ")),
-    dob: Yup.string().required(getRequiredText("dob")),
-    email: Yup.string().required(getRequiredText("Email Address")),
-    recoveryEmail: Yup.string().required(getRequiredText("recoveryEmail")),
-    phone: Yup.string().required(getRequiredText("phone")),
-    
-  
+  firstName: Yup.string().required(getRequiredText("firstName")),
+  lastName: Yup.string().required(getRequiredText("lastName")),
+
+  gender: Yup.string().required(getRequiredText(" gender ")),
+  dob: Yup.string().required(getRequiredText("dob")),
+
+  email: Yup.string().email(text.EMAIL_VALID).required(text.EMAIL_REQUIRED),
+  password: Yup.string()
+    .min(8)
+    .max(30)
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#%&])(?=.{8,})/,
+      text.PASSWORD_SUGGESTION
+    )
+    .required(text.PASSWORD_REQUIRED),
+  recoveryEmail: Yup.string().email(text.EMAIL_VALID).required(getRequiredText("recoveryEmail")),
+  phone: Yup.string()
+    .min(10)
+    .max(10)
+    .matches(/^[9][678][0-9]{8}$/, "Invalid phone number format")
+    .required(getRequiredText("phone")),
 });
