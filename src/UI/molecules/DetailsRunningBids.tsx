7230ddc52505cffd026433e84ea97aeaa647a491
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-12 17:03:34 +0545

    fix: changed the overall ui

--- src/UI/molecules/DetailsRunningBids.tsx
+++ src/UI/molecules/DetailsRunningBids.tsx
@@ -7,29 +7,29 @@ import BidderBar from "./BidderBar";
 import HighestBidBar from "./HighestBidBar";
 
 export default () => {
-	const location = useLocation();
-	const bid: Bid = location.state;
-	const [tokens, setTokens] = useState<any>([]);
-	//* this useEffect can be used anywhere, where we need realtime updates.
-	//! don't forget to import the firebase modules
+  const location = useLocation();
+  const bid: Bid = location.state;
+  const [tokens, setTokens] = useState<any>([]);
+  //* this useEffect can be used anywhere, where we need realtime updates.
+  //! don't forget to import the firebase modules
 
-	useEffect(() => {
-		const q = query(collection(db, "bidToken"), where("bidId", "==", bid._id));
-		const unsubscribe = onSnapshot(q, (querySnapshot) => {
-			setTokens(querySnapshot.docs.map((doc: any) => doc.data()));
-		});
-		//* cleanup
-		return () => {
-			unsubscribe();
-		};
-	}, []);
+  useEffect(() => {
+    const q = query(collection(db, "bidToken"), where("bidId", "==", bid._id));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      setTokens(querySnapshot.docs.map((doc: any) => doc.data()));
+    });
+    //* cleanup
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
-	return (
-		<section className='detailsRunningBids_section'>
-			{tokens.map((item: any, index: any) => {
-				return <BidderBar {...item} />;
-			})}
-			<HighestBidBar />
-		</section>
-	);
+  return (
+    <section className="detailsRunningBids_section">
+      <HighestBidBar />
+      {tokens.map((item: any, index: any) => {
+        return <BidderBar {...item} />;
+      })}
+    </section>
+  );
 };
