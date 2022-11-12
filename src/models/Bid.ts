9d41cfa6a45df93f7b52a3dcf4e53e0e25f33dfa
Jeewan Ghimire <jeewanghimire31@gmail.com>
2022-11-12 17:03:13 +0545

    fix: changes in type

--- src/models/Bid.ts
+++ src/models/Bid.ts
@@ -2,85 +2,85 @@ import { plainToClass } from "class-transformer";
 import Creative from "./Creative";
 
 class Meta {
-    highestToken: HighestToken;
-    constructor(highestToken: HighestToken) {
-        this.highestToken = highestToken;
-    }
-
+  highestToken: HighestToken;
+  constructor(highestToken: HighestToken) {
+    this.highestToken = highestToken;
+  }
 }
 
 class HighestToken {
-    value: number;
-    userId: string;
+  value: number;
+  userId: string;
 
-    constructor(value: number, userId: string) {
-        this.value = value
-        this.userId = userId
-    }
+  constructor(value: number, userId: string) {
+    this.value = value;
+    this.userId = userId;
+  }
 }
 
-
-
 class Bid {
-    _id: string;
-    statusMessage: string;
-    initialToken: string;
-    endDate: Date;
-    bidStory: string;
-    startDate: Date;
-    specification: string;
-    status: string;
-    minimumTokenRaise: number;
-    category: string;
-    brand: string;
-    meta: Meta;
-    tokenCurrency: string;
-    creative: Creative[]
-    userId: string;
-    name: string;
+  _id: string;
+  statusMessage: string;
+  initialToken: string;
+  endDate: Date;
+  bidStory: string;
+  startDate: Date;
+  specification: string;
+  status: string;
+  minimumTokenRaise: number;
+  category: string;
+  brand: string;
+  meta: Meta;
+  tokenCurrency: string;
+  creative: Creative[];
+  userId: string;
+  name: string;
+  highestTokenValue: number;
 
-    constructor(
-        id: string,
-        statusMessage: string,
-        initialToken: string,
-        endDate: Date,
-        bidStory: string,
-        startDate: Date,
-        specification: string,
-        status: string,
-        minimumTokenRaise: number,
-        category: string,
-        brand: string,
-        meta: Meta,
-        tokenCurrency: string,
-        creative: Creative[],
-        userId: string,
-        name: string
-    ) {
-        this._id = id
-        this.statusMessage = statusMessage
-        this.initialToken = initialToken
-        this.endDate = endDate
-        this.bidStory = bidStory
-        this.startDate = startDate
-        this.specification = specification
-        this.status = status
-        this.minimumTokenRaise = minimumTokenRaise
-        this.category = category
-        this.brand = brand
-        this.meta = meta
-        this.tokenCurrency = tokenCurrency
-        this.creative = creative
-        this.userId = userId
-        this.name = name
-    }
+  constructor(
+    id: string,
+    statusMessage: string,
+    initialToken: string,
+    endDate: Date,
+    bidStory: string,
+    startDate: Date,
+    specification: string,
+    status: string,
+    minimumTokenRaise: number,
+    category: string,
+    brand: string,
+    meta: Meta,
+    tokenCurrency: string,
+    creative: Creative[],
+    userId: string,
+    name: string,
+    highestTokenValue: number
+  ) {
+    this._id = id;
+    this.statusMessage = statusMessage;
+    this.initialToken = initialToken;
+    this.endDate = endDate;
+    this.bidStory = bidStory;
+    this.startDate = startDate;
+    this.specification = specification;
+    this.status = status;
+    this.minimumTokenRaise = minimumTokenRaise;
+    this.category = category;
+    this.brand = brand;
+    this.meta = meta;
+    this.tokenCurrency = tokenCurrency;
+    this.creative = creative;
+    this.userId = userId;
+    this.name = name;
+    this.highestTokenValue = highestTokenValue;
+  }
 
-    static plainToInstance(plainData: object): Bid {
-        return plainToClass(Bid, plainData);
-    }
-    static plainToInstances(plainData: object[]): Bid[] {
-        return plainToClass(Bid, plainData);
-    }
+  static plainToInstance(plainData: object): Bid {
+    return plainToClass(Bid, plainData);
+  }
+  static plainToInstances(plainData: object[]): Bid[] {
+    return plainToClass(Bid, plainData);
+  }
 }
 
 export default Bid;
